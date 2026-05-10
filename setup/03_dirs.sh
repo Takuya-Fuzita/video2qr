d3cc541e2dab2e1a2eeeeca2c4585640a2fad376
Takuya-Fuzita <xxxxxxxx+Takuya-Fuzita@users.noreply.github.com>
2026-05-10 18:57:24 +0900

    mod group

--- setup/03_dirs.sh
+++ setup/03_dirs.sh
@@ -5,8 +5,8 @@ source "$(dirname "$0")/common.sh"
 
 echo "[03_dirs] creating directories..."
 
-sudo install -d -o "${APP_USER}" -g "${APP_USER}" "${LOG_DIR}"
-sudo install -d -o "${APP_USER}" -g "${APP_USER}" "${SAVE_DIR}"
-sudo install -d -o "${APP_USER}" -g "${APP_USER}" "${LIB_DIR}"
+sudo install -d -m 775 -o "${APP_USER}" -g "${APP_GROUP}" "${LOG_DIR}"
+sudo install -d -m 775 -o "${APP_USER}" -g "${APP_GROUP}" "${SAVE_DIR}"
+sudo install -d -m 775 -o "${APP_USER}" -g "${APP_GROUP}" "${LIB_DIR}"
 
 echo "[03_dirs] done"
