cf791c46a135ef3c9da3b1591c86f094e7821ca2
Takuya Fuzita <tfujita1341@gmail.com>
2026-05-11 21:56:00 +0900

    Update 04_python.sh  add pip freeze

--- setup/04_python.sh
+++ setup/04_python.sh
@@ -6,10 +6,10 @@ source "$(dirname "$0")/common.sh"
 echo "[04_python] installing python packages..."
 
 sudo -u "${APP_USER}" "${VENV_DIR}/bin/pip" install \
-    opencv-python-headless \
-    pytesseract \
-    qrcode \
-    numpy \
-    Pillow
+    opencv-python-headless==4.13.0.92 \
+    pytesseract==0.3.13 \
+    qrcode==8.2 \
+    numpy==2.4.4 \
+    pillow==12.2.0
 
 echo "[04_python] done"
